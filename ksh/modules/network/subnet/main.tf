83d3e3d4c9e9fbedc306543eff450a6459407aa9
kimseonhyeok123 <221978403+seonhyeok1@users.noreply.github.com>
2025-08-16 21:11:50 +0900

    refactor(terraform): azs
    - availability_zone = var.azs[each.key]는 Terraform이 string → number 자동 변환

--- ksh/modules/network/subnet/main.tf
+++ ksh/modules/network/subnet/main.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "this" {
   for_each                = { for idx, cidr in var.cidr : tostring(idx) => cidr }
   vpc_id                  = var.vpc_id
   cidr_block              = each.value
-  availability_zone       = var.azs[each.key]
+  availability_zone       = var.azs[tonumber(each.key)]
   map_public_ip_on_launch = var.public
 
   tags = merge(
