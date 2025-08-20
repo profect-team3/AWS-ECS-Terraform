2e5be6a504caededb1d10e5d8d5e3a6303a614c6
yun1270 <modo000127@gmail.com>
2025-08-20 15:12:41 +0900

    chore(terraform): edge 파일 분리 및 수정 - api gateway, route53, waf 분리 - alb 보안그룹 삭제 -> data 변수

--- yun/stacks/20-edge/output.tf
+++
@@ -1,5 +0,0 @@
-# output "apigw_url"   { value = module.apigw.invoke_url }
-# output "edge_fqdn"   { value = module.route53.record_fqdn }
-# output "nlb_dns"     { value = module.nlb.nlb_dns_name }
-# output "alb_dns"     { value = module.alb.alb_dns_name }
-# output "alb_tgs"     { value = module.alb.target_group_arns }
