5ad361a9801d74b905d1054b201ae5560103020d
yun1270 <modo000127@gmail.com>
2025-08-22 17:21:41 +0900

    chore(terraform): esc namespace output 추가

--- modules/compute/ecs-cluster/output.tf
+++ modules/compute/ecs-cluster/output.tf
@@ -1,1 +1,2 @@
 output "cluster_arn" { value = aws_ecs_cluster.this.arn }
+output "namespace" { value = aws_service_discovery_private_dns_namespace.svc.name }
--- stacks/50-compute/output.tf
+++ stacks/50-compute/output.tf
@@ -1,4 +1,4 @@
 output "ecr_repository_url"  { value = module.ecr.repository_urls }
-output "cluster_arn"          { value = module.ecs_cluster.cluster_arn }
+output "cluster_arn"         { value = module.ecs_cluster.cluster_arn }
+output "namespace"           { value = module.ecs_cluster.namespace }
 output "task_definition_arns"{ value = module.ecs_task.task_definition_arns }
-
